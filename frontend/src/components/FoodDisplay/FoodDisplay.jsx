2bbcff10085877ace5baa7f6a318034ce1bdc7a7
Mansoor-P <munaf6007@gmail.com>
2024-03-16 21:02:11 +0530

    filtering food  items

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,23 +3,26 @@ import './FoodDisplay.css'
 import { StoreContext } from "../../context/StoreContext";
 import Fooditem from "../FoodItem/Fooditem";
 
-const FoodDisplay = () => {
+const FoodDisplay = ({category}) => {
   const { food_list } = useContext(StoreContext);
   return (
     <div className="food-display" id="food-display">
       <h2>Top Dishesh Near You</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          return (
-            <Fooditem
-              key={index}
-              id={item._id}
-              name={TextMetrics.name}
-              description={item.description}
-              price={item.price}
-              image={item.image}
-            />
-          );
+          if(category==="All"||category===item.category){
+            return (
+              <Fooditem
+                key={index}
+                id={item._id}
+                name={TextMetrics.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
+            );
+          }
+          
         })}
       </div>
     </div>
