0d29c43257e756d0c58192ca6bea2057beeca851
Mansoor-P <munaf6007@gmail.com>
2024-03-16 19:33:05 +0530

    header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order Your favorite Food Here</h2>
+        <p>Choose From a diverse menu featuring a declarable array of dishesh crafted with the finest   ingredients and culinary   expertise. Our Aim is to Satisfy your craving s and elivate your dining experiences.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
