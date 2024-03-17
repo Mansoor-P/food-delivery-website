bbae0dda7381ebb45c6076b8cdb26a7fc2869754
Mansoor-P <munaf6007@gmail.com>
2024-03-17 11:00:47 +0530

    cart page ui funtionality

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({setShowLogin}) => {
   const[menu,setMenu]=useState("menu");
   return (
     <div className="navbar">
-      <img className="logo" src={assets.logo} alt="logo" />
+     <Link to='/'> <img className="logo" src={assets.logo} alt="logo" /></Link>
       <ul className="navbar-menu">
         <Link to="/" onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
         <a   href="#explore-menu"   onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
@@ -16,7 +16,8 @@ const Navbar = ({setShowLogin}) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
+          <Link to='/cart'> <img src={assets.basket_icon} alt="" /></Link>
+         
           <div className="dot"></div>
         </div>
         <button onClick={()=>setShowLogin(true)}>Sign in</button>
